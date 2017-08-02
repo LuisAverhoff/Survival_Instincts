518d0ad3d85804590e941b05ee1e5fdba4ff8e45
Luis Averhoff <laver008@fiu.edu>
2017-08-02 01:50:19 -0400

    Added a UI element to show the current time of day as time progresses.

---
+++ Assets/Scripts/DayNightCycle/CalculateGameTime.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CalculateGameTime : MonoBehaviour
+{
+    [SerializeField] private DayNightController dayNightController;
+    [SerializeField] private Text timeString;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        float currentTime = dayNightController.getCurrentTimeOfDay();
+        timeString.text = constructTimeString(currentTime);
+	}
+
+    private string constructTimeString(float currentTime)
+    {
+        //Is it am of pm?
+        string AMPM = "";
+        float minutes = ((currentTime) - (Mathf.Floor(currentTime))) * 60.0f;
+
+        if (currentTime <= 12.0f)
+        {
+            AMPM = "AM";
+
+        }
+        else
+        {
+            AMPM = "PM";
+        }
+
+        //Make the final string
+        return Mathf.Floor(currentTime).ToString() + " : " + minutes.ToString("00") + " " + AMPM;
+    }
+}
