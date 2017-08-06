9fda312e5dddecbd50e4cfbcfb512fc157731af2
Luis Averhoff <laver008@fiu.edu>
2017-08-06 01:29:48 -0400

    Added Three radial sliders to the minimap. The health, thirst and slider UI elements encompasses the survival system that I will be creating.

---
+++ Assets/Scripts/Minimap/MinimapRadialBar.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MinimapRadialBar : MonoBehaviour
+{
+    [SerializeField]private Image radialBar;
+    [SerializeField]private float radialValue = 0;
+    [SerializeField]private float maxRadialValue = 100;
+    [SerializeField]private float maxRadialRotation = 180;
+
+    void Awake()
+    {
+        setRadialBar();
+    }
+
+    public void setRadialBar()
+    {
+        float amount = (radialValue / maxRadialValue) * maxRadialRotation / 360.0f;
+        radialBar.fillAmount = amount;
+    }
+
+    public void reduceRadialValue(float amountToReduceBy)
+    {
+        radialValue -= amountToReduceBy;
+    }
+
+    public float getRadialValue()
+    {
+        return radialValue;
+    }
+
+    public float getMaxRadialValue()
+    {
+        return maxRadialValue;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerVitals.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+public class PlayerVitals : MonoBehaviour
+{
+    [SerializeField]private GameObject healthRadialBar;
+    [SerializeField]private GameObject thirstRadialBar;
+    [SerializeField]private GameObject hungerRadialBar;
+
+    private MinimapRadialBar healthSlider;
+    private MinimapRadialBar thirstSlider;
+    private MinimapRadialBar hungerSlider;
+
+    [SerializeField]private float healthFallRate;
+    [SerializeField]private float thirstFallRate;
+    [SerializeField]private float hungerFallRate;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        healthSlider = healthRadialBar.GetComponent<MinimapRadialBar>();
+        thirstSlider = thirstRadialBar.GetComponent<MinimapRadialBar>();
+        hungerSlider = hungerRadialBar.GetComponent<MinimapRadialBar>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (hungerSlider.getRadialValue() <= 0 && thirstSlider.getRadialValue() <= 0)
+        {
+            healthSlider.reduceRadialValue(Time.deltaTime / healthFallRate * 2);
+        }
+        else if(hungerSlider.getRadialValue() <= 0 || thirstSlider.getRadialValue() <= 0)
+        {
+            healthSlider.reduceRadialValue(Time.deltaTime / healthFallRate);
+        }
+
+        healthSlider.setRadialBar();
+
+        if(healthSlider.getRadialValue() <= 0)
+        {
+            killCharacter();
+        }
+
+        if(hungerSlider.getRadialValue() >= 0)
+        {
+            hungerSlider.reduceRadialValue(Time.deltaTime / hungerFallRate);
+        }
+        else if(hungerSlider.getRadialValue() <= 0)
+        {
+            hungerSlider.reduceRadialValue(0);
+        }
+        else if(hungerSlider.getRadialValue() >= hungerSlider.getMaxRadialValue())
+        {
+            hungerSlider.reduceRadialValue(hungerSlider.getMaxRadialValue());
+        }
+
+        hungerSlider.setRadialBar();
+
+        if (thirstSlider.getRadialValue() >= 0)
+        {
+            thirstSlider.reduceRadialValue(Time.deltaTime / thirstFallRate);
+        }
+        else if (thirstSlider.getRadialValue() <= 0)
+        {
+            thirstSlider.reduceRadialValue(0);
+        }
+        else if (thirstSlider.getRadialValue() >= thirstSlider.getMaxRadialValue())
+        {
+            thirstSlider.reduceRadialValue(thirstSlider.getMaxRadialValue());
+        }
+
+        thirstSlider.setRadialBar();
+    }
+
+    private void killCharacter()
+    {
+        Debug.Log("You are dead!!!");
+    }
+}
